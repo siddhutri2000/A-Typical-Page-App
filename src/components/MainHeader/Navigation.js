7baa3cf99054b841281e755370b62dc2f2d3bd2c
Sid <siddhu.tri2000@gamil.com>
2022-01-16 19:24:45 +0530

    useEffect Added

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,6 +1,5 @@
-import React from 'react';
-
-import classes from './Navigation.module.css';
+import React from "react";
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
   return (
